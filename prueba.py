db62d6252fce55f16d175a18083db333d4041066
Javier Montes <fmontesestrabon@safareyes.es>
2023-11-08 12:23:02 +0100

    Tercer commit mio

--- prueba.py
+++ prueba.py
@@ -1,6 +1,9 @@
-frase = "Tal vez descubra la verdad al comparar las mentiras"
+frase = "Tal vez descubra la mentira al comparar las mentiras"
 frase = frase.split()
 for i in frase:
-    frase1 = i[::-1]
+    frase1 = i[::-3]
     print(frase1)
+for i in range(len(frase)):
+    print("A")
+
 
